c6997b70b44451c95241947fe5272a084ddd2126
tusharvashista <72988406+tusharvashista@users.noreply.github.com>
2025-03-16 07:24:34 +0000

    Update settings.py

--- cyberxp_project/settings.py
+++ cyberxp_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)ktlfig*%porxm=#-srqz2^)=6!c#ahmsj&a+-bx(2snib#zrq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['CyberXP.pythonanywhere.com']
 
 
 # Application definition
